67482f445117e3175921a57a8db112c787a59f93
GDRiveros <gonzalodaniel.r17@gmail.com>
2022-10-07 12:52:38 -0300

    Presentación de los modelos con __str__

--- AppAvanci/models.py
+++ AppAvanci/models.py
@@ -5,11 +5,19 @@ from django.db import models
 class Curso(models.Model):
     nombre = models.CharField(max_length=40)
     camada = models.IntegerField()
+    fecha_inicio = models.DateField(null=True)
+
+    def __str__(self):
+        return f"({self.camada}) {self.nombre}"
 
 class Estudiante(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+    
 
 class Profesor(models.Model):
     nombre = models.CharField(max_length=30)
@@ -17,8 +25,15 @@ class Profesor(models.Model):
     email = models.EmailField()
     profesion = models.CharField(max_length=30)
 
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
 
 class Entregable(models.Model):
     nombre = models.CharField(max_length=30)
     fecha_de_entrega = models.DateField()
     entregado = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.nombre}. Se entrega el día {self.fecha_de_entrega}"
+        
